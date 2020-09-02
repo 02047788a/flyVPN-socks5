0f46d071e56680c6dd7e903eadf4235f68f2b7db
jimmy947788 <jimmy.wu@daedalus.com.tw>
2020-09-02 18:31:16 +0800

    自動檢測撥號flyVPN

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-VPN_NAME="Thailand 02"
-flyvpn login
-echo "socks5" | flyvpn connect "$VPN_NAME"
+VPN_NAME="Japan 41"
+USNERNAME="proxyuser"
+PASSWORD="88958895"
+current_ip=$(curl  -s -v -x socks5://$USNERNAME:$PASSWORD@127.0.0.1:1080 ifconfig.me)
+
+if [ -z "$current_ip" ]; then
+    killall flyvpn
+    echo "not use flyVPN. (current_ip=$current_ip)"
+    flyvpn login
+    echo "socks5" | flyvpn connect "$VPN_NAME"
+else
+    echo "useing flyVPN connect to $VPN_NAME. (current_ip=$current_ip)"
+fi
--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 USNERNAME="proxyuser"
 PASSWORD=""
+current_ip=$(curl -v -x socks5://$USNERNAME:$PASSWORD@127.0.0.1:1080 ifconfig.me)
 
-curl -v -x socks5://$USNERNAME:$PASSWORD@127.0.0.1:1080 https://myip.com.tw/
+if [ -z "$current_ip" ]; then
+    echo "Not connect flyVPN. (company ip: $company_ip)"
+else
+    echo "Current is $current_ip connecting flyVPN. (company ip: $company_ip)"
+fi
